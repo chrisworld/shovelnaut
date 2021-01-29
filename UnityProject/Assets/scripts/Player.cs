7cad64082a7c80734ee9325c8266501a1ca2df99
dominikims <76172478+dominikims@users.noreply.github.com>
2021-01-29 21:47:09 +0100

    made playermovement

--- UnityProject/Assets/scripts/Player.cs
+++ UnityProject/Assets/scripts/Player.cs
@@ -4,23 +4,66 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed;
     private Vector2 targetPosition;
+
+    Rigidbody2D rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
         targetPosition = new Vector2(0.0f, 0.0f);
+
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Mouse Controls
+        /*
         if (Input.GetMouseButtonDown(0))
         {
             targetPosition = Input.mousePosition;
             targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(targetPosition.x, targetPosition.y, 0.0f));
         }
         this.transform.position = Vector2.MoveTowards(this.transform.position, targetPosition, speed * Time.deltaTime);
+        */
+
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        rigidbody.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+
+
+       /* if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rigidbody.velocity = new Vector2(-moveSpeed, 0);
+
+            //TODO: Animation left
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rigidbody.velocity = new Vector2(moveSpeed, 0);
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigidbody.velocity = new Vector2(0, moveSpeed);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rigidbody.velocity = new Vector2(0, -moveSpeed);
+
+        }*/
         
+
+
+
     }
 }
