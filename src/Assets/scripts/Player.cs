b9e70772b181e5a49f0d5e725490aebba8f6eaac
Stefan Lontschar <stefan_lontschar@gmx.at>
2021-01-31 15:35:26 +0100

    build adaptions. player sanity check

--- src/Assets/scripts/Player.cs
+++ src/Assets/scripts/Player.cs
@@ -140,7 +140,8 @@ public class Player : MonoBehaviour
             {
                 aim.hasHiddenPart = false;
             }
-            OnPlayerUsedShovel();
+            if (OnPlayerUsedShovel != null)
+                OnPlayerUsedShovel();
             shovelMovesCount--;
         }
 
