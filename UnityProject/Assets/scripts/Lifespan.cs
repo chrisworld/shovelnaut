d4ca176da10c0e8e25da9044e08c83ac0bc87e70
dominikims <76172478+dominikims@users.noreply.github.com>
2021-01-31 12:08:52 +0100

    MadeFootStepScript

---
+++ UnityProject/Assets/scripts/Lifespan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifespan : MonoBehaviour
+{
+
+    public float lifespan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyObject", lifespan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
